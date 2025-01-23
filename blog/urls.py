81b73e6914142746f6c0b541a46ad502422963b2
Carlos Neves <c.nsm.714@gmail.com>
2025-01-23 20:30:20 +0000

    add profile and other pages related to profile features

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
-         views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
-         views.comment_delete, name='comment_delete'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('create/', views.create_post, name='create_post'),
+    path('edit/<slug:slug>/', views.edit_post, name='edit_post'),
+    path('delete/<slug:slug>/', views.delete_post, name='delete_post'),
+    path('profile/', views.profile, name='profile'),
+    path('approve_comment/<int:comment_id>/', views.approve_comment, name='approve_comment'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
